782bd25f17b38448c819906e531041bd3b4b070b
Claire Wong <cwon0185@student.monash.edu>
2026-05-17 22:15:30 +1000

    modifying animation walk by implementing Javascript over CSS for stronger control

--- src/components/ui/Socrates.jsx
+++ src/components/ui/Socrates.jsx
@@ -1,25 +1,26 @@
 import { useState, useEffect } from 'react'
-import socratesRight from '../assets/socrates-right.png'
-import socratesMid from '../assets/socrates-mid.png'
-import socratesStand from '../assets/socrates-stand.png'
+import socratesRight from '../../assets/socrates-right.png'
+import socratesMid from '../../assets/socrates-right-mid-stride.png'
+import socratesStand from '../../assets/socrates-right-stand.png'
 import './Socrates.css'
 
-const frames = [socratesStand, socratesMid, socratesRight, socratesMid]
+const frames = [socratesStand, socratesRight, socratesMid, socratesRight]
 
 export default function Socrates() {
     const [current, setCurrent] = useState(0)
 
     useEffect(() => {
         const interval = setInterval(() => {
-            console.log('frame changing to:', (prev + 1) % frames.length)
             setCurrent(prev => (prev + 1) % frames.length)
-        }, 300)
+        }, 600)
         return () => clearInterval(interval)
     }, [])
 
     return (
-        <div className="socrates-walk">
-            <img src={frames[current]} alt="Socrates" className="socrates-frame" />
+        <div className="socrates-container">
+            <div className="socrates-walk">
+                <img src={frames[current]} alt="Socrates" className="socrates-frame" />
+            </div>
         </div>
     )
 }
