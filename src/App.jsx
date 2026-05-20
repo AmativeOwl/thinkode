bbc6e226d63c11f86182e52986eb240650c6a732
Claire Wong <cwon0185@student.monash.edu>
2026-05-20 20:34:20 +1000

    extracted onAdd handler into a new function in App.jsx for readability

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,33 @@ import AddProblemModal from "./components/shared/AddProblemModal"
 import './App.css'
 
 export default function App() {
+  // destructing problems and related functions from useProblems
   const { problems, addProblem, updateProblem, deleteProblem } = useProblems()
   const [showAddModal, setShowAddModal] = useState(false)
   const [recentProblemIds, setRecentProblemIds] = useState([])
   const navigate = useNavigate()
 
+  // selects problem by id, adding it to the list of recent problems if it's not already there and navigates to it 
   function handleSelectProblem(id) {
     setRecentProblemIds(prev => prev.includes(id) ? prev : [...prev, id])
     navigate(`/problem/${id}`)
   }
 
+  // adds a new problem and navigates to it, closing the pop-up add modal
+  const handleAdd = async (fields) => {
+    const newProblem = await addProblem(fields)
+    if (newProblem) handleSelectProblem(newProblem.id)
+    setShowAddModal(false)
+  }
+
+  // removes a problem from the list of recent problems, navigating back to the start page if no recent problems left 
   function removeFromRecent(id) {
     const updated = recentProblemIds.filter(p => p !== id)
     setRecentProblemIds(updated)
     if (updated.length === 0) navigate('/')
   }
 
+  // capturing the recent problems based on the list of recent problem ids, filtering out any that might be deleted via Boolean evaluation
   const recentProblems = recentProblemIds.map(id => problems.find(p => p.id === id)).filter(Boolean)
 
   return (
@@ -52,11 +63,7 @@ export default function App() {
         <AddProblemModal
           problems={problems}
           onClose={() => setShowAddModal(false)}
-          onAdd={async (fields) => {
-            const newProblem = await addProblem(fields)
-            if (newProblem) handleSelectProblem(newProblem.id)
-            setShowAddModal(false)
-          }}
+          onAdd={handleAdd}
         />
       )}
     </>
